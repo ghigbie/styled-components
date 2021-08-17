293a81c03380c7c5d435c3638cec11224d93f258
ghigbie <gehigbie@gmail.com>
2021-08-17 19:04:12 -0400

    Initial commit

--- src/App.js
+++ src/App.js
@@ -3,21 +3,9 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ padding: '2rem', backgroundColor: 'grey'}}>
+      <h1 className="title">Styled Components</h1>
+      <button className="btn">Click Me!</button>
     </div>
   );
 }
